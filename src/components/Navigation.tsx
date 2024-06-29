efae7cbb197148ba650c420f0408275b1d25f8c6
Lukas Mikulastik <lukas.mikulastik@qest.cz>
2024-06-29 11:15:12 +0200

    hp video

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,23 +1,26 @@
+import {useEffect, useState} from "react";
+
 const Navigation = () => {
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        // Add scroll event listener when component mounts
+        window.addEventListener('scroll', handleScroll);
+
+        // Clean up event listener when component unmounts
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); // Empty dependency array ensures this effect runs only once
+
+    const navBackground = scrollY < 200 ? 'bg-transparent' : 'bg-black';
     return (
-        <nav className={'fixed top-0 p-4'}>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="about">About</a>
-                </li>
-                <li>
-                    <a href="offer">Offer</a>
-                </li>
-                <li>
-                    <a href="lectors">Lectors</a>
-                </li>
-                <li>
-                    <a href="contact">Contact</a>
-                </li>
-            </ul>
+        <nav className={`fixed top-0 p-4 w-screen space-x-4 flex justify-between items-center transition-all duration-500 ${navBackground}`}>
+            <img src={'src/assets/logo.svg'} alt={'FitImpulz'} className={'w-20 h-auto'} />
         </nav>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,30 +1,38 @@
+import Navigation from "../components/Navigation.tsx";
 
 const HomePage = () => {
 
     return (
-        <div className={'relative bg-black'}>
-            <div className={'bg-black h-screen w-screen relative'}>
-                <video autoPlay loop muted
-                       className={'w-screen h-screen object-cover absolute top-0 left-0 z-0 blur-sm filter backdrop-filter'}>
-                    <source src={'/hp-video.mp4'} type={'video/mp4'}/>
-                </video>
-                <div
-                    className={'absolute top-0 left-0 w-full h-full bg-black opacity-70 flex flex-col justify-end p-8 pb-24'}>
-                    <h1 className={'text-5xl text-white '}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Vivamus at eros libero.</h1>
+        <>
+            <div className={'relative bg-black'}>
+                <div className={'bg-black h-screen w-screen relative'}>
+                    <video autoPlay loop muted
+                           className={'w-screen h-screen object-cover absolute top-0 left-0 z-0 blur-sm filter backdrop-filter'}>
+                        <source src={'/hp-video.mp4'} type={'video/mp4'}/>
+                    </video>
+                    <div
+                        className={'absolute top-0 left-0 w-full h-full bg-black opacity-70 flex flex-col justify-end p-8 pb-24'}>
+                        <h1 className={'text-white '}>Nějaká hodně chytrá motivující hláška 💪</h1>
+                    </div>
+                </div>
+                <div className={'pl-4 p-12'}>
+                    <p>At FitImpulz, we put our members' fitness first. We've designed a facility complemented by
+                        today’s
+                        most
+                        popular fitness equipment. Whether your fitness plan is geared for weight loss or body building,
+                        our
+                        equipment will suit your needs. Our fitness center features a wide range of classes and
+                        well-maintained
+                        amenities like locker rooms and showers. Visit us to discover our range of offerings.
+
+                    </p>
+                </div>
+                <div className={'bg-white flex justify-center items-center py-24 px-8'}>
+                    <p className={'text-2xl text-black font-extrabold'}>Lorem Ipsum</p>
                 </div>
             </div>
-            <div className={'pl-4 p-12'}>
-                <p>At FitImpulz, we put our members' fitness first. We've designed a facility complemented by today’s
-                    most
-                    popular fitness equipment. Whether your fitness plan is geared for weight loss or body building, our
-                    equipment will suit your needs. Our fitness center features a wide range of classes and
-                    well-maintained
-                    amenities like locker rooms and showers. Visit us to discover our range of offerings.
-
-                </p>
-            </div>
-        </div>
+            <Navigation />
+        </>
     )
 }
 
